6aca2f0179324b6bef9f39a6a72684314353e916
Md Salman <mdsalman.backend@playniya.com>
2025-08-02 16:46:40 +0530

    init cicd playground

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="h-screen bg-emerald-500">
+      <div className="bg-red-500 text-white">how are you doign</div>
+      <div className="bg-red-500 text-white">hello</div>
+      <div className="bg-red-500"></div>
+    </div>
+  );
+};
+
+export default App;
